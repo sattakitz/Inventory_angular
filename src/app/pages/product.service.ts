a644d3e0074e8aac36172dbfcb91eb074a5078db
sattakit <sattakit.kacs60@ubru.ac.th>
2025-03-10 17:40:26 +0700

    product modal

---
+++ src/app/pages/product.service.ts
@@ -0,0 +1,11 @@
+// product.service.ts
+import { Injectable } from '@angular/core';
+import { Product } from './product.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ProductService {
+
+
+}
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import NavbarComponent from "../../layouts/navbar/navbar.component";
+import { RouterLink } from '@angular/router';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-product',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
+
+export class ProductComponent implements OnInit {
+  isModalOpen: boolean = false;
+
+  openModal() {
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+  }
+
+  ngOnInit(): void {
+
+  }
 
 }
